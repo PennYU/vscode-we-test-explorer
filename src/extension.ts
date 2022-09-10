190624c4674b5d7512b45c7ebd620f37614815ee
Yu Zhenpin <yuzhenpin@gmail.com>
2022-09-10 21:02:45 +0800

    just for fun

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import { TestHub, testExplorerExtensionId } from 'vscode-test-adapter-api';
+import { WeTestController } from './weTestController';
+import { WeTreeViewProvider } from './weTreeViewProvider';
+
+let testHub: TestHub | undefined;
+let controller: WeTestController | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-we-test-explorer" is now active!');
+	const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
+	const provider = new WeTreeViewProvider(context.extensionUri);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-we-test-explorer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-we-test-explorer!');
-	});
+	if (testExplorerExtension) {
 
-	context.subscriptions.push(disposable);
+		testHub = testExplorerExtension.exports;
+		controller = new WeTestController(provider);
+		testHub.registerTestController(controller);
+	}
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(WeTreeViewProvider.viewType, provider));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vscode-we-test-explorer.addColor', () => {
+			provider.addColor();
+		}));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vscode-we-test-explorer.clearColors', () => {
+			provider.clearColors();
+		}));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (testHub && controller) {
+		testHub.unregisterTestController(controller);
+	}
+}
---
+++ src/weTestController.ts
@@ -0,0 +1,101 @@
+import * as vscode from 'vscode';
+import { WeTreeViewProvider } from './weTreeViewProvider';
+import { TestController, TestAdapter, TestSuiteInfo, TestInfo } from 'vscode-test-adapter-api';
+
+export class WeTestController implements TestController {
+
+	private readonly disposables = new Map<TestAdapter, { dispose(): void }[]>();
+
+	private statusBarItem: vscode.StatusBarItem;
+	private passedTests = 0;
+	private failedTests = 0;
+
+	constructor(readonly weTreeViewProvider: WeTreeViewProvider) {
+
+		this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+		this.statusBarItem.show();
+
+		this.statusBarItem.command = 'test-explorer.run-all';
+	}
+
+	registerTestAdapter(adapter: TestAdapter): void {
+		
+		const adapterDisposables: { dispose(): void }[] = [];
+		this.disposables.set(adapter, adapterDisposables);
+
+		adapterDisposables.push(adapter.tests(testLoadEvent => {
+
+			if (testLoadEvent.type === 'started') {
+
+				this.statusBarItem.text = 'Loading tests...';
+
+			} else { // testLoadEvent.type === 'finished'
+
+				const rootSuite = testLoadEvent.suite;
+				const testCount = rootSuite ? countTests(rootSuite) : 0;
+				this.statusBarItem.text = `Loaded ${testCount} tests`;
+				if (rootSuite) {
+					rootSuite.children.forEach(suite => {
+						const child = suite as TestSuiteInfo;
+						this.weTreeViewProvider.addColor();
+						if (child && child.children) {
+							child.children.forEach(() => {
+								this.weTreeViewProvider.addColor();
+							});
+						}
+					});
+				}
+			}
+		}));
+
+		adapterDisposables.push(adapter.testStates(testRunEvent => {
+
+			if (testRunEvent.type === 'started') {
+
+				this.statusBarItem.text = 'Running tests: ...';
+				this.passedTests = 0;
+				this.failedTests = 0;
+
+			} else if (testRunEvent.type === 'test') {
+
+				if (testRunEvent.state === 'passed') {
+					this.passedTests++;
+				} else if (testRunEvent.state === 'failed') {
+					this.failedTests++;
+				}
+
+				this.statusBarItem.text = `Running tests: ${this.passedTests} passed / ${this.failedTests} failed`;
+
+			} else if (testRunEvent.type === 'finished') {
+
+				this.statusBarItem.text = `Tests: ${this.passedTests} passed / ${this.failedTests} failed`;
+
+			}
+		}));
+	}
+
+	unregisterTestAdapter(adapter: TestAdapter): void {
+
+		const adapterDisposables = this.disposables.get(adapter);
+		if (adapterDisposables) {
+
+			for (const disposable of adapterDisposables) {
+				disposable.dispose();
+			}
+
+			this.disposables.delete(adapter);
+		}
+	}
+}
+
+function countTests(info: TestSuiteInfo | TestInfo): number {
+	if (info.type === 'suite') {
+		let total = 0;
+		for (const child of info.children) {
+			total += countTests(child);
+		}
+		return total;
+	} else { // info.type === 'test'
+		return 1;
+	}
+}
